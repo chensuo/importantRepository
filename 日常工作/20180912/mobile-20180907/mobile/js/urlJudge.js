e600376fcb5cb140ce0bfd6906e04b321e5ccdb7
chensuo <chensuo@broadtext.com.cn>
2018-09-19 09:10:24 +0800

    更新20180918工作日志和mobile源码备份

---
+++ 日常工作/20180912/mobile-20180907/mobile/js/urlJudge.js
@@ -0,0 +1,21 @@
+/**
+ * Created by cs on 2018/9/18.
+ */
+/*根据url判断当前页面tabbar选中情况(发布于JEECMS时，需要更改indexOf里字符串)*/
+$(function(){
+    var  url = window.location.pathname;
+    var $tabbarItem = $('.weui-tabbar__item');
+    if(url.indexOf("index.html") >= 0){
+        $($tabbarItem[0]).addClass('weui-bar__item--on');
+    }
+    if(url.indexOf("discovery.html") >= 0){
+        $($tabbarItem[1]).addClass('weui-bar__item--on');
+    }
+    if(url.indexOf("guide.html") >= 0){
+        $($tabbarItem[2]).addClass('weui-bar__item--on');
+    }
+    if(url.indexOf("mine.html") >= 0){
+        $($tabbarItem[3]).addClass('weui-bar__item--on');
+    }
+});
+
