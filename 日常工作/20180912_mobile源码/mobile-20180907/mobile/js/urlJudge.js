fcad38aa4a25d832d8b5abd4f1fd4159a40c32b3
chensuo <chensuo@broadtext.com.cn>
2018-09-25 16:24:27 +0800

    持续更新mobile模板，更新20180925工作日报

--- 日常工作/20180912_mobile源码/mobile-20180907/mobile/js/urlJudge.js
+++ 日常工作/20180912_mobile源码/mobile-20180907/mobile/js/urlJudge.js
@@ -5,18 +5,20 @@
 $(function(){
     var  url = window.location.pathname;
     var $tabbarItem = $('.weui-tabbar__item');
-    if(url.indexOf("index.html") >= 0){
-        $($tabbarItem[0]).addClass('weui-bar__item--on');
-    }
+
     if(url.indexOf("discovery.html") >= 0 || url.indexOf("discovery-content.html") >= 0 || url.indexOf("discovery-content_2.html") >= 0){
         $($tabbarItem[1]).addClass('weui-bar__item--on');
+        return false;
     }
     if(url.indexOf("guide.html") >= 0){
         $($tabbarItem[2]).addClass('weui-bar__item--on');
+        return false;
     }
     if(url.indexOf("mine.html") >= 0){
         $($tabbarItem[3]).addClass('weui-bar__item--on');
+        return false;
     }
+        $($tabbarItem[0]).addClass('weui-bar__item--on');
 });
 
 
