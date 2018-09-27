0fdf45fefac92cd904c98b944488cc2afb77ae71
chensuo <chensuo@broadtext.com.cn>
2018-09-27 17:26:04 +0800

    持续更新mobile模板，备份jeecms布置好的代码，上传20180927工作日报

--- 日常工作/20180912_mobile源码/mobile-20180907/jeecms代码备份/mobile/js/urlJudge.js
+++ 日常工作/20180912_mobile源码/mobile-20180907/jeecms代码备份/mobile/js/urlJudge.js
@@ -1,24 +1,23 @@
-/**
- * Created by cs on 2018/9/18.
- */
+
 /*根据url判断当前页面tabbar选中情况(发布于JEECMS时，需要更改indexOf里字符串)*/
 $(function(){
     var  url = window.location.pathname;
     var $tabbarItem = $('.weui-tabbar__item');
 
-    if(url.indexOf("discovery.html") >= 0 || url.indexOf("discovery-content.html") >= 0 || url.indexOf("discovery-content_2.html") >= 0){
+    if(url.indexOf("tzgg/") >= 0 || url.indexOf("xw/") >= 0){
         $($tabbarItem[1]).addClass('weui-bar__item--on');
-        return false;
+         return false;
     }
-    if(url.indexOf("guide.html") >= 0){
+    if(url.indexOf("zn") >= 0){
         $($tabbarItem[2]).addClass('weui-bar__item--on');
-        return false;
+         return false;
     }
-    if(url.indexOf("mine.html") >= 0){
+   if(url.indexOf("wd") >= 0){
         $($tabbarItem[3]).addClass('weui-bar__item--on');
-        return false;
+       return false;
     }
+
     $($tabbarItem[0]).addClass('weui-bar__item--on');
+
+
 });
-
-
