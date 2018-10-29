1fa12d3b85230716e84f916bfb30edda253ee395
chensuo <chensuo@broadtext.com.cn>
2018-10-29 18:08:14 +0800

    重新设计mobile模板，备份旧版本

---
+++ 日常工作/2018_9月/20180912_mobile源码/mobile-20180907(20181029改版)/mobile/js/urlJudge.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cs on 2018/9/18.
+ */
+/*根据url判断当前页面tabbar选中情况(发布于JEECMS时，需要更改indexOf里字符串)*/
+$(function(){
+    var  url = window.location.pathname;
+    var $tabbarItem = $('.weui-tabbar__item');
+
+    if(url.indexOf("discovery.html") >= 0 || url.indexOf("discovery-content.html") >= 0 || url.indexOf("discovery-content_2.html") >= 0){
+        $($tabbarItem[1]).addClass('weui-bar__item--on');
+        return false;
+    }
+    if(url.indexOf("mine.html") >= 0 || url.indexOf("myfavorite.html") >= 0){
+        $($tabbarItem[2]).addClass('weui-bar__item--on');
+        return false;
+    }
+    if(url.indexOf("search_result.html") >= 0){
+        $tabbarItem.removeClass('weui-bar__item--on');
+        return false;
+    }
+    $($tabbarItem[0]).addClass('weui-bar__item--on');
+});
