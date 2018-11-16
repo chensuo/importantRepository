ca5cef884a8d37916edcf0c7e021b0fdb02f19ee
chensuo <chensuo@broadtext.com.cn>
2018-11-16 18:00:54 +0800

    提交20181112-20181116工作周报

--- 日常工作/2018_9月/20180912_mobile源码/mobile-20180907(20181029改版)/mobile/js/global.js
+++ 日常工作/2018_9月/20180912_mobile源码/mobile-20180907(20181029改版)/mobile/js/global.js
@@ -1,8 +1,12 @@
-/*定义全局变量*/
+/*全局变量*/
 $(function(){
-    //获取token
-    document.tokenUrl='/ssc';
+    //获取token前缀
+    document.tokenUrl='/ssc/';
 
-//获取申请数，代办数，代办列表，申请列表，审批历史
-    document.showUrl='/sscmanage';
+    //获取代办数、申请数、代办列表、申请列表、审批历史 数据链接(包括表单的跳转) 获取数据的URL前缀
+    document.showUrl='/sscmanage/';
+
+    //cms页面之间 跳转链接前缀
+    document.lnkUrl = '/ssc/';
 });
+
